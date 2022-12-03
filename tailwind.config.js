0bafe7c147181d9c941ff9290e72d107a4fee40e
Kehinde Malagu <Kehindemalagu@gmail.com>
2022-12-03 08:43:30 +0100

    Initial Setup

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        outfit: "outfit , sans-serif"
+      }
+    },
+  },
+  plugins: [],
+}
